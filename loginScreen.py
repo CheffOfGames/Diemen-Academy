a729942df6e3463209618eda5f0688834620c4c3
Steve <stevevandijk@icloud.com>
2021-05-17 13:04:47 +0200

    Loginscrren and profilescreen
    created layout

--- loginScreen.py
+++ loginScreen.py
@@ -4,3 +4,14 @@ from blankScreen import *
 class LoginScreen(Screen):
     def __init__(self, root: Tk, frame: Frame, screens: tuple):
         super().__init__(root, frame, screens)
+        self.canvas.create_rectangle((self.width/2.3), (self.height/3), (self.width/1.7), (self.height/2))
+
+        self.entry1 = Entry(root)
+        self.entry2 = Entry(root)
+        label1 = Label(root,text="Student Number")
+        label1.place(x=(self.width/2.3), y=(self.height/2.5))
+        self.entry1.place(x=(self.width/2), y=(self.height/2.5))
+
+        label2 = Label(root,text="Password")
+        label2.place(x=(self.width/2.3),y= (self.height/2.3))
+        self.entry2.place(x=(self.width/2), y=(self.height/2.3))
--- profileScreen.py
+++ profileScreen.py
@@ -4,3 +4,7 @@ from blankScreen import *
 class ProfileScreen(Screen):
     def __init__(self, root: Tk, frame: Frame, screens: tuple):
         super().__init__(root, frame, screens)
+        self.label = Label(self.root, text="Name \nLast Name \nAdres")
+        self.label.place(x =(self.width/4) , y= (self.height/7))
+        self.canvas.create_rectangle((self.width/15), (self.height/7), (self.width/6), (self.height/2.5))
+        
