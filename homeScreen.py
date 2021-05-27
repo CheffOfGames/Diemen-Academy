82ffb7e53e44f4a18394b5148330f898e1c471ce
Steve <stevevandijk@icloud.com>
2021-05-27 10:39:28 +0200

    Homescreen

--- homeScreen.py
+++ homeScreen.py
@@ -16,10 +16,30 @@ class HomeScreen(Screen):
             self.changeScreen("Login")
         
         if self.usertype == 0: # Student
-            pass
+             button_grades = Button(root,text="Grades",command = '')
+             button_schedule = Button(root,text="Schedule",command = '')
+             button_profile = Button(root,text="Profile",command = '')
+             button_enroll = Button(root,text="Grades",command = '')
+
+             button_grades.place(x= (self.width/3), y= (self.height/3))
+             button_schedule.place(x= (self.width/3*2), y= (self.height/3))
+             button_profile.place(x= (self.width/3), y= (self.height/3*2))
+             button_enroll.place(x= (self.width/3*2), y= (self.height/3*2))
 
         elif self.usertype == 1: # Teacher
-            pass
+            button_subjects = Button(root,text="Subjects",command = '')
+            button_scheduleteacher = Button(root,text="Schedule",command = '')
+
+            button_subjects.place(x= (self.width/3), y= (self.height/3))
+            button_scheduleteacher.place(x= (self.width/3*2), y= (self.height/3))
 
         elif self.usertype == 2: # Administrator
-            pass
+            button_student = Button(root,text="Student",command = '')
+            button_teacher = Button(root,text="Teacher",command = '')
+            button_gradesadmin = Button(root,text="Grades",command = '')
+            button_course = Button(root,text="Course",command = '')
+
+            button_student.place(x= (self.width/3), y= (self.height/3))
+            button_teacher.place(x= (self.width/3*2), y= (self.height/3))
+            button_gradesadmin.place(x= (self.width/3), y= (self.height/3*2))
+            button_course.place(x= (self.width/3*2), y= (self.height/3*2))
