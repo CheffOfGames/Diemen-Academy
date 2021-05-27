09c45e1ed462eca97f2e43c317427d93746155d5
Jeffrey <jeffreyhollander1999@gmail.com>
2021-05-27 15:05:17 +0200

    Removed UI_root

--- main.py
+++ main.py
@@ -18,37 +18,11 @@ x_pos, y_pos = int(width / 24), int(height / 8)
 root.resizable(0,0)
 root.geometry(f"{width}x{height}+{x_pos}+{y_pos}")
 
-# UI shit
-ui_height, ui_width, ui_x_pos, ui_y_pos = int(width / 8), int(width / 4), int(x_pos*1.5 + width), (y_pos)
-ui_root = Tk()
-ui_root.resizable(0,0)
-ui_root.geometry(f"{ui_width}x{ui_height}+{ui_x_pos}+{ui_y_pos}")
-
-# Set frames
-ui_frame = Frame(ui_root, width=ui_width, height=ui_height)
-ui_frame.pack()
-
 # Current page
 current_page = LoginScreen(root, screens, database)
 
 # Change page
 screens_list = ["Home", "Enroll", "Grades", "Login", "Profile", "Schedule", "Subject"]
 
-var = StringVar(ui_root)
-start_val = 0
-for i in range(len(screens)) :
-    if screens[screens_list[i]] == type(current_page) :
-        start_val = i
-var.set(screens_list[start_val])
-
-screens_menu = OptionMenu(ui_frame, var, *screens_list)
-screens_menu.config(width=int(ui_width/2))
-screens_menu.pack(side="top")
-
-def callback(*args) :
-    current_page.changeScreen(var.get())
-var.trace("w", callback)
-
 # End of file
 root.mainloop()
-ui_root.mainloop()
