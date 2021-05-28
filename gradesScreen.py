241c4f84018c65b5f1554f1d257cddb11bd00e71
Julia <juliaveronique@gmail.com>
2021-05-28 14:14:49 +0200

    connected database info with grade- and profilescreen

--- gradesScreen.py
+++ gradesScreen.py
@@ -6,26 +6,31 @@ class GradesScreen(Screen):
     def __init__(self, root: Tk, screens: dict, database):
         super().__init__(root, screens, database)  
         self.root.title("Grades Screen")
-        exams = [1,2,3,4,5]
         place = self.height/6
-        self.cursor.execute("SHOW TABLES") 
 
-        tables = self.cursor.fetchall() 
-        print (tables[0][0])
+        self.cursor.execute(f" select a.course_name, b.grade, b.passed, c.date from course a, result b, exam c\
+                                where b.fk_student_id = {self.current_user}\
+                                and b.fk_exam_id = c.id\
+                                and a.id = c.fk_course_id ") 
+        exams = self.cursor.fetchall() 
 
         for i in range (len(exams)):
             if i%2 == 0:
                 self.canvas.create_rectangle(-1,place-self.height/20,self.width+1,place+self.height/20, fill="light grey", outline="")
-            label_subject = Label(root,text="subject")
+            label_subject = Label(root,text=exams[i][0])
             label_subject.config(font=("Courier", 12))
             label_subject.place(x=self.width/12, y=place,anchor="center"  )
 
-            label_grade = Label(root,text="grade")
+            if exams[i][2] == 0:
+                label_grade = Label(root,text=exams[i][1], fg='red')
+            else:
+                label_grade = Label(root,text=exams[i][1])
+                
             label_grade.config(font=("Courier", 12))
             label_grade.place(x=self.width/2, y=place,anchor="center"   )
             self.canvas.create_oval((self.width/2)-self.height/24, place-self.height/24,(self.width/2)+self.height/24, place+self.height/24)
 
-            label_date = Label(root,text="date")
+            label_date = Label(root,text=exams[i][3])
             label_date.config(font=("Courier", 12))
             label_date.place(x=9*self.width/10, y=place,anchor="center"  )
 
--- profileScreen.py
+++ profileScreen.py
@@ -5,7 +5,29 @@ class ProfileScreen(Screen):
     def __init__(self, root: Tk, screens: dict, database):
         super().__init__(root, screens, database)  
         self.root.title("Profile Screen")
-        self.label = Label(self.root, text="Name \nLast Name \nAdres")
-        self.label.place(x =(self.width/4) , y= (self.height/7))
+
+        self.cursor.execute(f"  select a.first_name, a.last_name, a.date_of_birth, a.nationality, a.gender, a.email, a.start_year, b.study_name, a.studycounselor, c.street, c.house_number, c.postal_code, c.city, c.phone\
+                                from student a, study b, adress c\
+                                where b.id=a.fk_study_id\
+                                and c.id=a.fk_adress_id\
+                                and a.id={self.current_user}; ") 
+
+        profile_info = self.cursor.fetchall()[0]
+
+        self.personalinfo = Label(self.root, text=f"Name: {profile_info[0]} {profile_info[1]} \nBorn: {profile_info[2]}\
+                           \nNationality: {profile_info[3]} \nGender: {profile_info[4]}", justify='left')
+        if profile_info[5] == '  ':
+            self.schoolinfo = Label(self.root, text=f"Start Year: {profile_info[6]}\nStudy: {profile_info[7]}\nStudy Counselor: {profile_info[8]}", justify='left')                  
+        else:
+            self.schoolinfo = Label(self.root, text=f"Email: {profile_info[5]} \nStart Year: {profile_info[6]}\nStudy: {profile_info[7]}\nStudy Counselor: {profile_info[8]}", justify='left')                  
+
+        self.contactinfo = Label(self.root, text=f"{profile_info[9]} {profile_info[10]} \n{profile_info[11]} \n{profile_info[12]} \n{profile_info[13]}", justify='left')                  
+
+        self.personalinfo.place(x =(self.width/4) , y= (self.height/7))
+        self.schoolinfo.place(x =(self.width/4) , y= (3*self.height/7))
+        self.contactinfo.place(x =(self.width/4) , y= (5*self.height/7))
+
         self.canvas.create_rectangle((self.width/15), (self.height/7), (self.width/6), (self.height/2.5))
-        
+
+        # [('Pjotr', 'Tjaikovsky', datetime.date(1992, 10, 10), 'Russian', 'M', '  ',
+        #  2017, 'M.Planck', 'Mathematical Engineering', 'oigf', 45, '9785 BG', 'Groningen', '454-3455')]
