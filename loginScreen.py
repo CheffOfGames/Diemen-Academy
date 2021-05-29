3406eb7db5b88a6c4a837aa24b7d2a361bc21dd8
Jeffrey <jeffreyhollander1999@gmail.com>
2021-05-29 22:29:36 +0200

    Login revision continued

--- loginScreen.py
+++ loginScreen.py
@@ -14,7 +14,7 @@ class LoginScreen(Screen):
 
         # Enter password field
         self.pass_label = Label(root,text="Password:").place(x=(self.width/2.70),y= (self.height/2.3))
-        self.pass_entry = Entry(root)
+        self.pass_entry = Entry(root, show="*")
         self.pass_entry.place(x=(self.width/2), y=(self.height/2.3))
 
         # Submit button
@@ -32,15 +32,36 @@ class LoginScreen(Screen):
                     self.current_usertype = 1
                     self.changeScreen("Home", self.current_usertype)
                 else :
-                    pass
+                    self.error_popup("Please enter a real id")
+
             except :
-                pass # Error message
+                self.error_popup("Please enter a real id")
 
         elif self.current_user[0] == "a" and str(self.pass_entry.get()).lower() == "root":
             self.current_user = self.current_user[1:]
             self.current_usertype = 2
             self.changeScreen("Home", self.current_usertype)
+        
         else:
-            self.current_usertype = 0
-            self.changeScreen("Home", self.current_usertype)
-            
+            try :
+                self.cursor.execute(f"select id from teacher where id={self.current_user}")
+
+                if len(self.cursor.fetchall()) != 0 :
+                    self.current_usertype = 0
+                    self.changeScreen("Home", self.current_usertype)
+                else :
+                    self.error_popup("Please enter a real id")
+            except :
+                self.error_popup("Please enter a real id")
+
+    def error_popup(self, message) :
+        self.user_entry.delete(0, 'end')
+        self.pass_entry.delete(0, 'end')
+        error_root = Tk()
+        error_root.resizable(0,0)
+        error_root.title("ERROR")
+        width, height = int(error_root.winfo_screenwidth()*.2), int(error_root.winfo_screenheight()*.2)
+        error_root.geometry(f"{width}x{height}+{int((error_root.winfo_screenwidth()-width)/2)}+{int((error_root.winfo_screenheight()-height)/2)}")
+        frame = Frame(error_root, width=width, height=height)
+        Label(frame, text=f"{message}").place(x=width/2, y=height/2, anchor="center")
+        Button(frame, text="Try again", command=lambda: error_root.destroy())
